97734f18617f987e857765b5916038cc9ac9d967
kapilsuyati <kkgoyal@suyati.com>
2022-10-03 17:37:24 +0530

    update signup UI

--- lib/Signup.dart
+++ lib/Signup.dart
@@ -14,133 +14,142 @@ class _SignupState extends State<Signup> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("Signup")),
-        body: Container(
-          padding: const EdgeInsets.all(20),
-          child: Form(
-            key: formKey, //key for form
-            child: Column(children: [
-              const SizedBox(
-                    height: 20.0,
-                  ),
-              TextFormField(
-                decoration:  InputDecoration(
-                  hintText: "Username",
-                  suffixIcon: const Icon(Icons.email),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0)),
-                  ),
-                validator: (value) {
-                  if (value!.isEmpty ||
-                      !RegExp(r'^[a-z A-Z]+$').hasMatch(value)) {
-                    //allow upper and lower case alphabets and space
-                    return "Enter Correct Name";
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-
-              const SizedBox(
-                    height: 20.0,
-                  ),
-
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Email",
-                  suffixIcon: const Icon(Icons.email),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.only(
+              bottom: 50), // This is for scrolling from bottom
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              key: formKey, //key for form
+              child: Column(children: [
+                const SizedBox(
+                  height: 0.0,
                 ),
-                validator: (value) {
-                  if (value!.isEmpty ||
-                      !RegExp(r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$')
-                          .hasMatch(value)) {
-                    //r'^[0-9]{10}$' pattern plain match number with length 10
-                    return "Enter Correct Phone Number";
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-
-              const SizedBox(
-                    height: 20.0,
+                SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: Image.asset(
+                    'assets/yoga.png',
+                    fit: BoxFit.fill,
                   ),
-
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Phone number",
-                  suffixIcon: const Icon(Icons.phone),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
                 ),
-                validator: (value) {
-                  if (value!.isEmpty ||
-                      !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
-                          .hasMatch(value)) {
-                    //r'^[0-9]{10}$' pattern plain match number with length 10
-                    return "Enter Correct Email Address";
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-
-              const SizedBox(
-                    height: 20.0,
-                  ),
-
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Password",
-                  suffixIcon: const Icon(Icons.visibility_off),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+                 const SizedBox(
+                  height: 20.0,
                 ),
-                validator: (value) {
-                  if (value!.isEmpty ||
-                      !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
-                          .hasMatch(value)) {
-                    //r'^[0-9]{10}$' pattern plain match number with length 10
-                    return "Enter Correct Email Address";
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-
-              const SizedBox(
-                    height: 20.0,
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Username",
+                    suffixIcon: const Icon(Icons.email),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
                   ),
-
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Confirm Password",
-                  suffixIcon: const Icon(Icons.visibility_off),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-                ),
-                validator: (value) {
-                  if (value!.isEmpty ||
-                      !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
-                          .hasMatch(value)) {
-                    //r'^[0-9]{10}$' pattern plain match number with length 10
-                    return "Enter Correct Email Address";
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-
-              const SizedBox(
-                    height: 20.0,
-                  ),
-
-              ElevatedButton(
-                  onPressed: () {
-                    if (formKey.currentState!.validate()) {
-                      //Check if form data are valid
-                      // your process task ahead if all data are valid
+                  validator: (value) {
+                    if (value!.isEmpty ||
+                        !RegExp(r'^[a-z A-Z]+$').hasMatch(value)) {
+                      //allow upper and lower case alphabets and space
+                      return "Enter Correct Name";
+                    } else {
+                      return null;
                     }
                   },
-                  child: const Text("Sign up"))
-            ]),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    suffixIcon: const Icon(Icons.email),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty ||
+                        !RegExp(r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$')
+                            .hasMatch(value)) {
+                      //r'^[0-9]{10}$' pattern plain match number with length 10
+                      return "Enter Correct Phone Number";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Phone number",
+                    suffixIcon: const Icon(Icons.phone),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty ||
+                        !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                            .hasMatch(value)) {
+                      //r'^[0-9]{10}$' pattern plain match number with length 10
+                      return "Enter Correct Email Address";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    suffixIcon: const Icon(Icons.visibility_off),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty ||
+                        !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                            .hasMatch(value)) {
+                      //r'^[0-9]{10}$' pattern plain match number with length 10
+                      return "Enter Correct Email Address";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Confirm Password",
+                    suffixIcon: const Icon(Icons.visibility_off),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty ||
+                        !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                            .hasMatch(value)) {
+                      //r'^[0-9]{10}$' pattern plain match number with length 10
+                      return "Enter Correct Email Address";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        //Check if form data are valid
+                        // your process task ahead if all data are valid
+                      }
+                    },
+                    child: const Text("Sign up"))
+              ]),
+            ),
           ),
         ));
   }
